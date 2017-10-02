3c97504df88715a4bc27842ac480d7d889416d77
Alexey Orlenko <eaglexrlnk@gmail.com>
2017-10-02 20:51:40 +0300

    fix(bridge): make binding.c more robust

--- crates/mhp-node/src/binding.c
+++ crates/mhp-node/src/binding.c
@@ -2,9 +2,6 @@
 
 extern void mhp_initialize(void);
 
-#define DECLARE_NAPI_PROPERTY(name, func) \
-  { (name), 0, (func), 0, 0, 0, napi_default, 0 }
-
 napi_value initialize(napi_env env, napi_callback_info info) {
   mhp_initialize();
 
@@ -14,9 +11,38 @@ napi_value initialize(napi_env env, napi_callback_info info) {
   return result;
 }
 
+#define MHP_PROPERTY_MAP(V)                                                   \
+  V(initialize, initialize)
+
+#ifdef __GNUC__
+#define UNLIKELY(expr) __builtin_expect(!!(expr), 0)
+#else
+#define UNLIKELY(expr) (expr)
+#endif
+
+#define ASSERT_NAPI_OK(expr)                                                  \
+  if (UNLIKELY((expr) != napi_ok)) {                                          \
+    napi_throw_error(env, NULL, "Assertion failed: (" #expr ") != napi_ok");  \
+    return NULL;                                                              \
+  }
+
 napi_value init(napi_env env, napi_value exports) {
-  napi_property_descriptor desc = DECLARE_NAPI_PROPERTY("initialize", initialize);
-  napi_define_properties(env, exports, 1, &desc);
+#define V(name, func)                                                         \
+  { #name, NULL, func, NULL, NULL, NULL, napi_default, NULL },
+
+  napi_property_descriptor descriptors[] = {
+    MHP_PROPERTY_MAP(V)
+  };
+#undef V
+
+  napi_status status = napi_define_properties(
+      env,
+      exports,
+      sizeof(descriptors) / sizeof(descriptors[0]),
+      descriptors);
+
+  ASSERT_NAPI_OK(status);
+
   return exports;
 }
 
