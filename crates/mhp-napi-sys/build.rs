1c9f46bedb95ffdaaa936a99a82eb3fc925f7d98
Alexey Orlenko <eaglexrlnk@gmail.com>
2017-10-03 04:48:04 +0300

    fix(napi-sys): try to fix the Windows build

--- crates/mhp-napi-sys/build.rs
+++ crates/mhp-napi-sys/build.rs
@@ -3,9 +3,11 @@ extern crate which;
 
 use std::env;
 use std::path::PathBuf;
+use std::process::Command;
 
 use which::which;
 
+#[cfg(unix)]
 fn find_node_api_header() -> Result<String, &'static str> {
     Ok(
         which("node")?
@@ -21,6 +23,39 @@ fn find_node_api_header() -> Result<String, &'static str> {
             .ok_or("Could not convert path to string")?
             .to_owned(),
     )
+}
+
+#[cfg(windows)]
+fn find_node_api_header() -> Result<String, &'static str> {
+    Command::new("cmd")
+        .args(&["/C", "node-gyp", "install"])
+        .output()
+        .or_else(Err("Could not run node-gyp install"))?;
+
+    let node_version_output = Command::new("cmd")
+        .args(&["/C", "node", "--version"])
+        .output()
+        .or_else(Err("Could not run node --version"))?
+        .stdout;
+
+    let node_version = String::from_utf8(node_version_output)
+        .or_else(Err("Could not parse node --version output"))?
+        .trim();
+
+    let home_dir =
+        env::var("OUT_DIR").or_else("Could not find user directory")?;
+
+    let path = PathBuf::from(home_dir)
+        .as_path()
+        .join(".node-gyp")
+        .join(node_version)
+        .join("include")
+        .join("node")
+        .join("node_api.h")
+        .to_str()
+        .ok_or("Could not convert path to string")?;
+
+    Ok(path.to_owned())
 }
 
 fn main() {
