24dc9beb4bf9bde9a90d8fb73793ec537b2833b0
Alexey Orlenko <eaglexrlnk@gmail.com>
2017-10-04 11:04:11 +0300

    fix(napi): implement std::error::Error for NapiError

--- crates/mhp-napi/src/result.rs
+++ crates/mhp-napi/src/result.rs
@@ -1,3 +1,7 @@
+use std::error::Error;
+use std::fmt;
+use std::fmt::Display;
+
 #[derive(Clone, Debug)]
 pub enum NapiError {
     InvalidArg(String),
@@ -13,6 +17,74 @@ pub enum NapiError {
     Cancelled(String),
     EscapeCalledTwice(String),
     UnknownError(String),
+}
+
+impl Error for NapiError {
+    fn description(&self) -> &str {
+        match *self {
+            NapiError::InvalidArg(_) => "NapiError: invalid argument",
+            NapiError::ObjectExpected(_) => "NapiError: object expected",
+            NapiError::StringExpected(_) => "NapiError: string expected",
+            NapiError::NameExpected(_) => "NapiError: name expected",
+            NapiError::FunctionExpected(_) => "NapiError: function expected",
+            NapiError::NumberExpected(_) => "NapiError: number expected",
+            NapiError::BooleanExpected(_) => "NapiError: boolean argument",
+            NapiError::ArrayExpected(_) => "NapiError: array expected",
+            NapiError::GenericFailure(_) => "NapiError: generic failure",
+            NapiError::PendingException(_) => "NapiError: pending exception",
+            NapiError::Cancelled(_) => "NapiError: cancelled",
+            NapiError::EscapeCalledTwice(_) => "NapiError: escape called twice",
+            NapiError::UnknownError(_) => "NapiError: unknown error",
+        }
+    }
+}
+
+impl Display for NapiError {
+    fn fmt(&self, formatter: &mut fmt::Formatter) -> fmt::Result {
+        write!(formatter, "{} ", self.description())?;
+
+        match *self {
+            NapiError::InvalidArg(ref message) => {
+                write!(formatter, "({})", message)
+            }
+            NapiError::ObjectExpected(ref message) => {
+                write!(formatter, "({})", message)
+            }
+            NapiError::StringExpected(ref message) => {
+                write!(formatter, "({})", message)
+            }
+            NapiError::NameExpected(ref message) => {
+                write!(formatter, "({})", message)
+            }
+            NapiError::FunctionExpected(ref message) => {
+                write!(formatter, "({})", message)
+            }
+            NapiError::NumberExpected(ref message) => {
+                write!(formatter, "({})", message)
+            }
+            NapiError::BooleanExpected(ref message) => {
+                write!(formatter, "({})", message)
+            }
+            NapiError::ArrayExpected(ref message) => {
+                write!(formatter, "({})", message)
+            }
+            NapiError::GenericFailure(ref message) => {
+                write!(formatter, "({})", message)
+            }
+            NapiError::PendingException(ref message) => {
+                write!(formatter, "({})", message)
+            }
+            NapiError::Cancelled(ref message) => {
+                write!(formatter, "({})", message)
+            }
+            NapiError::EscapeCalledTwice(ref message) => {
+                write!(formatter, "({})", message)
+            }
+            NapiError::UnknownError(ref message) => {
+                write!(formatter, "({})", message)
+            }
+        }
+    }
 }
 
 pub type NapiResult<T> = Result<T, NapiError>;
